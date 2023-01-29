244a548707a61175344e6a5c0c4ff8a956593ebf
Anish Gyawali <anish.gyawali7@gmail.com>
2023-01-29 10:16:55 -0600

    added login component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Login from "./components/Login";
+
 function App() {
   return (
-    <div>
-      <div>react form</div>
-    </div>
+    <>
+      <Login/>
+    </>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Your logic to handle form submission goes here
+    // ...
+  };
+
+  const handleGoogleLogin = () => {
+    // Your logic to handle Google sign in goes here
+    // ...
+  };
+
+  const handleForgotPassword = () => {
+    // Your logic to handle forgot password goes here
+    // ...
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <span className="login-title">Login Form</span>
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input
+          className="form-control"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input
+          className="form-control"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="submit-button" type="submit">
+        Sign In
+      </button>
+
+      <button className="google-button" onClick={handleGoogleLogin}>
+        Sign In with Google
+        <span className="google-logo"></span>
+      </button>
+      <p className="forgot-password">
+        <a href="/#" onClick={handleForgotPassword}>
+          Forgot Password
+        </a>
+      </p>
+    </form>
+  );
+}
+export default Login;
